1b3bbfb9a9d423979c17dc53a217e2de91286d91
ckdgh <ckdgh@DESKTOP-JUUCKCV>
2026-01-03 15:46:39 +0900

    test1

--- java_control/src/day20260102/Test1.java
+++ java_control/src/day20260102/Test1.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Test1 {
 
 	public static void main(String[] args) {
+		
+	
 		 //if 주차장에 요금이 기본요금 1000원이다
 		 // 기본 시간은 30분이다.
 		 //10분당 100원씩 추가 예 39분은 1000원, 40분은 1100원
@@ -13,18 +15,45 @@ public class Test1 {
 		 //		4시간 12분 (252분) - 2600원
 		 //분을 적으면 요금이 얼마인지 나오게 하라
 		
-		  int charge = 1000;
-		 int addcharge = 100;
+		 int charge = 1000;
+		 int add;
 		 int fourhcharge = 2500;
-		 
-		 
-		 
+		 	
 		
-		 System.out.println("이용시간(분) : "+ time);
+		System.out.print("이용시간(분) : ");
 		Scanner kbd = new Scanner(System.in);
-		String time =kbd.nextLine();
+		int time =kbd.nextInt();
+		
+		for(add=100; add <= 100; add++ )
+		
+		if(time >= 1 && time < 40) {
+			System.out.println("이용요금 : "+charge);
+		}else if (time >= 40 && time < 50 ) {
+			System.out.println("이용요금 : "+add+charge);
+		}else if (time >= 50 && time < 60 ) {
+			System.out.println("이용요금 : "+(add+charge+100));
+		}else if (time >= 60 && time < 70 ) {
+			System.out.println("이용요금 : "+(add+charge+200));
+		}else if (time >= 70 && time < 80 ) {
+			System.out.println("이용요금 : "+(add+charge+300));			
+		}else if (time >= 80 && time < 90 ) {
+			System.out.println("이용요금 : "+(add+charge+400));
+		}else if (time >= 90 && time < 100 ) {
+			System.out.println("이용요금 : "+(add+charge+400));
+		}else if (time >= 100 && time < 110 ) {
+			System.out.println("이용요금 : "+(add+charge+400));
+	
+			
+		}
+//		}else {
+//			System.out.println("이용요금 :"+fourhcharge);
+			
+		}
+	 
+	 
+		
 		 
-		 
+	 
 	}
 
-}
+
---
+++ java_control/src/day20260102/Test1_1.java
@@ -0,0 +1,34 @@
+package day20260102;
+
+import java.util.Scanner;
+
+public class Test1_1 {
+
+	public static void main(String[] args) {
+		
+		int time;
+		int base = 1000;
+		int min = 30;
+		int addbase = 2500;
+		Scanner kbd = new Scanner(System.in);
+		
+		System.out.print("주차시간(분): ");
+		time = kbd.nextInt();
+
+		if(time > 30 ) {
+			for(min=40; time >= min; min+=10 ) {
+			base+= 100;	
+			}
+		} 
+		System.out.println("금액: "+base+"원");
+		 if(time > 240) {
+			for(min=250; time >= min; min+=10 ) {
+				addbase+= 100;
+		}
+		}
+		System.out.println("금액: "+addbase+"원");
+		
+
+	}
+
+}
