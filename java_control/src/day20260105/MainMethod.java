5eb0aeaafba4cbec1cc060e305362dd421706080
DW-203 <DW-203@DESKTOP-60N7HL8>
2026-01-05 10:56:43 +0900

    105

--- java_control/src/day20260105/MainMethod.java
+++ java_control/src/day20260105/MainMethod.java
@@ -4,7 +4,7 @@ public class MainMethod {
 
 	public static void main(String[] args) {
 		
-		int num = (int)Math.floor(Math.random()*10);
+		int num = (int)Math.floor(Math.random()*10)+1;
 		
 		System.out.println(num);
 
