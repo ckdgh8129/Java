f9181cb1b1524bd31a6db6bc6aa39a3f8f63b5fd
DW-203 <DW-203@DESKTOP-60N7HL8>
2026-01-05 10:48:49 +0900

    주차요금 0105 if문 사용

--- java_control/src/day20260102/Test1_1.java
+++ java_control/src/day20260102/Test1_1.java
@@ -6,29 +6,55 @@ public class Test1_1 {
 
 	public static void main(String[] args) {
 		
-		int time;
-		int base = 1000;
-		int min;
-		int addbase = 2500;
-		Scanner kbd = new Scanner(System.in);
+//		int time;
+//		int base = 1000;
+//		int min;
+//		int addbase = 2500;
+//		Scanner kbd = new Scanner(System.in);
+//		
+//		System.out.print("주차시간(분): ");
+//		time = kbd.nextInt();
+//
+//		if(time >= 240 ) {
+//			for(min=250; time >= min; min+=10 ) {
+//			addbase+= 100;	
+//			}
+//			System.out.println("금액: "+addbase+"원");
+//		}
+//		else if(time >= 30) {
+//			for(min=40; time >= min; min+=10 ) {
+//				base+= 100;
+//		}
+//			System.out.println("금액: "+base+"원");
+//		}
+//		else {
+//			System.out.println("금액: "+base+"원");
+//	}
+	
+		int cost = 1000; //기본요금
+		int inc = 100, incTime= 10;//10분당 100원
+		int time = 30; //기본시간 30분
 		
-		System.out.print("주차시간(분): ");
-		time = kbd.nextInt();
-
-		if(time >= 240 ) {
-			for(min=250; time >= min; min+=10 ) {
-			addbase+= 100;	
-			}
-			System.out.println("금액: "+addbase+"원");
+		//주차 시간 중 몇분 입력
+		Scanner scan = new Scanner(System.in);//시스템 으로 부터 입력된 키 값 받기
+		
+		System.out.println("총 몇분 주차 :");
+		int pTime = scan.nextInt();//키보드 입력 값 받아서 정수로 변환하여 변수에 저장
+		
+		pTime/=10; //pTime = pTime/10 -3
+	
+		if( pTime >= 24 ) { //4시간 이상 주차 했다면 pTime >= 240
+			cost = 2500; //4시간 이상에 대한 기본요금 변경
+			pTime -=24; //4시간 빼기, 4시간에 대한 요금은 2500원 나머지는 10분당 요금
+		}else if( pTime > 3 ) { //4시간 이상이 아니라면. 기본시간 30분으로 계산하여야한다.
+			pTime -= 3; //기본요금이 30분이니까 3빼기
+		}else { //최초 기본시간 안에서 주차 한 경우
+			pTime=0;
 		}
-		else if(time >= 30) {
-			for(min=40; time >= min; min+=10 ) {
-				base+= 100;
-		}
-			System.out.println("금액: "+base+"원");
-		}
-		else {
-			System.out.println("금액: "+base+"원");
+		// 주차요금 계산
+		int price = cost + pTime*100;
+		System.out.println("주차요금 : "+price+"원");
+	
 	}
-	}
+	
 }
---
+++ java_control/src/day20260105/MainMethod.java
@@ -0,0 +1,14 @@
+package day20260105;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		
+		int num = (int)Math.floor(Math.random()*10);
+		
+		System.out.println(num);
+
+
+	}
+
+}
