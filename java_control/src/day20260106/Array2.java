56890e40ad56de8a11a903e1c005e8cd935f1803
DW-203 <DW-203@DESKTOP-60N7HL8>
2026-01-06 17:36:34 +0900

    20060106

--- java_control/src/day20260106/Array2.java
+++ java_control/src/day20260106/Array2.java
@@ -13,7 +13,7 @@ public class Array2 {
 //				System.out.println("   "+num2[k]);
 //			}
 //		}
-
+//
 		int[] bingo = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
 		
 		for(int i=0; i<5; i++) {
@@ -21,11 +21,9 @@ public class Array2 {
 				System.out.println(bingo[i*5+k]);
 			}
 		}
-		
+	}
+	}
 		//1~100 까지 랜덤으로 중복없이 25개를 뽑아라
 		
-		
-		
-	}
 
-}
+
