7939c421ca2e3dbf3a5ec801fee23b91e43c037b
ckdgh <ckdgh@DESKTOP-JUUCKCV>
2026-01-08 21:45:00 +0900

    0108연습

--- java_control/src/day20260108/JavaClass2.java
+++ java_control/src/day20260108/JavaClass2.java
@@ -12,29 +12,34 @@ public class JavaClass2 {
 		movie.limitAge = 12;
 		movie.year = 2025;
 		
-		System.out.println(movie.title+" "+movie.dir+" "+movie.limitAge+" "+movie.year);
-
-		Movie moive2 = new Movie("굿포츈",2026);
-		
-		System.out.println(moive2.title+' '+moive2.year);
-		
+//		System.out.println(movie.title+" "+movie.dir+" "+movie.limitAge+" "+movie.year);
+//
+//		Movie moive2 = new Movie("굿포츈",2026);
+//		
+//		System.out.println(moive2.title+' '+moive2.year);
+//		
 		//Music 클래스와 객체 생성하여
 		// 음악제목, 가수, 재생시간(총몇분) 저장되게 하기
 		// 매개변수 있는 생성자 메서드로 데이터 저장하고
 		//출력
 		
-		Music music= new Music("어머님께","god",4);
-		System.out.print(music.musicName);
-		System.out.print("\t"+music.aut);
-		System.out.println("\t"+music.playTime+"분");
+//		Music music= new Music("어머님께","god",4);
+//		System.out.print(music.musicName);
+//		System.out.print("\t"+music.aut);
+//		System.out.println("\t"+music.playTime+"분");
+//		
+//		Music music2 = new Music();
+//		music2.aut2 = "gg";
+//		music2.aut = "ff";
+//		System.out.println(music2.aut2);
+//		System.out.println(music2.aut);
+//		
+		Wildsoul wildsoul = new Wildsoul("꼬리바람","어흥곰",50,4);
 		
-		Music music2 = new Music();
-		music2.aut2 = "gg";
-		music2.aut = "ff";
-		System.out.println(music2.aut2);
-		System.out.println(music2.aut);
-		
-		
-	}
+		System.out.println(wildsoul.tail);
+		System.out.println(wildsoul.bear);
+		System.out.println(wildsoul.lv);
+		System.out.println(wildsoul.sp);
+	
 
-}
+		} }
---
+++ java_control/src/day20260108/Wildsoul.java
@@ -0,0 +1,18 @@
+package day20260108;
+
+public class Wildsoul {
+
+	String tail;
+	String bear;
+	int lv;
+	int sp;
+
+	Wildsoul(String tail, String bear, int lv, int sp) {
+		this.tail = tail;
+		this.bear = bear;
+		this.lv = lv;
+		this.sp = sp;
+
+	}
+
+}
